db48a42a5d9b9963bd5121ae8ac2cb7d09eef068
alu0101109251 <alu0101109251@ull.edu.es>
2020-12-14 15:07:07 +0000

    Unit Testing

---
+++ src/test/java/es/ull/esit/TransportsTest.java
@@ -0,0 +1,38 @@
+package es.ull.esit;
+
+import es.ull.esit.factories.CruiseShipFactory;
+import es.ull.esit.factories.TransportFactory;
+import es.ull.esit.transports.Transport;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Transports Testing")
+public class TransportsTest {
+
+    /*private ArrayList<Transport> transports;
+
+    @BeforeEach
+    void setUp() {
+
+    }*/
+
+    @Test
+    @DisplayName("Class Getters")
+    void testFlightGetters() {
+        TransportFactory factory = new CruiseShipFactory();
+        Transport cruise = factory.createTransport();
+        cruise.setCoordinates(28.453323, -16.029710);
+
+        assertAll("Verify all attributes are set correctly",
+                () -> assertEquals("Cruise", cruise.getType(), "Transport Type"),
+                () -> assertEquals(28.453323, cruise.getLatitude(), "Transport Latitude"),
+                () -> assertEquals(-16.029710, cruise.getLongitude(), "Transport Longitude")
+        );
+    }
+}
