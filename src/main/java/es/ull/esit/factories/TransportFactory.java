0a1a05ee97d395dc291f66dacacc626f65174565
alu0101109251 <alu0101109251@ull.edu.es>
2020-12-14 14:47:28 +0000

    Doxygen

--- src/main/java/es/ull/esit/factories/TransportFactory.java
+++ src/main/java/es/ull/esit/factories/TransportFactory.java
@@ -2,6 +2,15 @@ package es.ull.esit.factories;
 
 import es.ull.esit.transports.Transport;
 
+/**
+ *
+ * @class TransportFactory
+ * @brief Interface to represent an Abstract Factory
+ *
+ * @details This class provides methods to be implemented by concrete factories.
+ *
+ */
+
 public interface TransportFactory {
     Transport createTransport();
 }
--- src/main/java/es/ull/esit/transports/Transport.java
+++ src/main/java/es/ull/esit/transports/Transport.java
@@ -1,14 +1,43 @@
 package es.ull.esit.transports;
 
-public abstract class Transport {
-    protected String type;
-    protected Double latitude;
-    protected Double longitude;
+/**
+ *
+ * @class Transport
+ * @brief Abstract class to represent a transport
+ *
+ * @details This class provides common methods and attributes shared
+ * between every transport.
+ *
+ */
 
+public abstract class Transport {
+    protected String type;          /**< Transport type. */
+    protected Double latitude;      /**< Current latitude. */
+    protected Double longitude;     /**< Current longitude. */
+
+    /**
+     * @brief Type Getter
+     * @return String -> transport type
+     */
     public String getType() { return type; }
+
+    /**
+     * @brief Latitude Getter
+     * @return Double -> latitude value
+     */
     public Double getLatitude() { return latitude; }
+
+    /**
+     * @brief Longitude Getter
+     * @return Double -> longitude value
+     */
     public Double getLongitude() { return longitude; }
 
+    /**
+     * @brief Coordinates Setter
+     * @param latitude -> new latitude value
+     * @param longitude -> new longitude value
+     */
     public void setCoordinates(Double latitude, Double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
