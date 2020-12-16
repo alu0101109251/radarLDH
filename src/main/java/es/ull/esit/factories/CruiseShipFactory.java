67b7868d478a41d5604a518d0fe01970551b4484
RICK4D <56073639+RICK4D@users.noreply.github.com>
2020-12-16 14:57:36 +0000

    Adding comments to CruiseShipFactory

--- src/main/java/es/ull/esit/factories/CruiseShipFactory.java
+++ src/main/java/es/ull/esit/factories/CruiseShipFactory.java
@@ -5,7 +5,17 @@ import es.ull.esit.transports.Transport;
 
 import java.awt.geom.Point2D;
 
+/**
+ *  @class CruiseShipFactory
+ *  @brief A concrete factory, which function is generate an CruiseShip
+ *
+ */
+
 public class CruiseShipFactory implements TransportFactory {
+    /**
+     * @brief Overrided method capable to create a new CruiseShip
+     * @return new CruiseShip
+     */
     @Override
     public Transport createTransport() {
         return new CruiseShip();
