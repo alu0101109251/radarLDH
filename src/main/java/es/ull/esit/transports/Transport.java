9a1c67f8f1be7519595315b58dfe9974cc41de2d
alu0101109251 <alu0101109251@ull.edu.es>
2020-12-16 13:55:27 +0000

    Changed program logic

--- src/main/java/es/ull/esit/transports/Transport.java
+++ src/main/java/es/ull/esit/transports/Transport.java
@@ -1,5 +1,8 @@
 package es.ull.esit.transports;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
 /**
  *
  * @class Transport
@@ -11,9 +14,16 @@ package es.ull.esit.transports;
  */
 
 public abstract class Transport {
-    protected String type;          /**< Transport type. */
-    protected Double latitude;      /**< Current latitude. */
-    protected Double longitude;     /**< Current longitude. */
+    protected String type;              /**< Transport type. */
+
+    protected Point2D startLocation;    /**< Start location of the transport path. */
+    protected Point2D endLocation;
+    protected Point2D currentLocation;
+
+    protected ArrayList<Point2D> path;
+
+    protected int currentPathIndex;
+
 
     /**
      * @brief Type Getter
@@ -25,21 +35,74 @@ public abstract class Transport {
      * @brief Latitude Getter
      * @return Double -> latitude value
      */
-    public Double getLatitude() { return latitude; }
+    public Double getLatitude() { return currentLocation.getX(); }
 
     /**
      * @brief Longitude Getter
      * @return Double -> longitude value
      */
-    public Double getLongitude() { return longitude; }
+    public Double getLongitude() { return currentLocation.getY(); }
+
+    public void move() {
+        if(currentPathIndex < path.size()) {
+            currentLocation.setLocation(path.get(currentPathIndex++));
+        }
+    }
+
+    public void setStartLocation(double latitude, double longitude) {
+        this.startLocation = new Point2D.Double(latitude, longitude);
+        setCurrentLocation(latitude, longitude);
+        currentPathIndex = 0;
+    }
+
+    public void setEndLocation(double latitude, double longitude) {
+        this.endLocation = new Point2D.Double(latitude, longitude);
+    }
 
     /**
-     * @brief Coordinates Setter
-     * @param latitude -> new latitude value
-     * @param longitude -> new longitude value
+     * @brief Current coordinates setter
+     * @param latitude -> current latitude location
+     * @param longitude -> current longitude location
      */
-    public void setCoordinates(Double latitude, Double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
+    public void setCurrentLocation(double latitude, double longitude) {
+        this.currentLocation = new Point2D.Double(latitude, longitude);
+    }
+
+
+    public void setPath() {
+        path.add(startLocation);
+
+        int xI = Math.round((float) startLocation.getX());
+        int yI = Math.round((float) startLocation.getY());
+
+        int xF = Math.round((float) endLocation.getX());
+        int yF = Math.round((float) endLocation.getY());
+
+        // Calculate straight line equation
+        float m = (float) (yF - yI) / (float) (xF - xI);
+        float c = yI - m*xI;
+
+        // Determine dependent variable
+        int xDelta = Math.abs(xF - xI);
+        int yDelta = Math.abs(yF - yI);
+
+        // Assign straight line points
+        if(xDelta >= yDelta) {
+            // y = mx + c
+
+            for(int x = Math.min(xI, xF), j = 0; x <= Math.max(xI, xF); x++, j++) {
+                int y = Math.round(m*x + c);
+                path.add(new Point2D.Double(x, y));
+            }
+        }
+        else {
+            // x = (y - c) / m
+
+            for(int y = Math.min(yI, yF), j = 0; y <= Math.max(yI, yF); y++, j++) {
+                int x = Math.round((y - c) / m);
+                path.add(new Point2D.Double(x, y));
+            }
+        }
+        path.add(endLocation);
     }
 }
