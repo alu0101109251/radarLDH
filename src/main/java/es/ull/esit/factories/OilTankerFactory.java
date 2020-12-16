8dbba9d5e333e34fc8fae4e28aa7ea457b7537c3
RICK4D <56073639+RICK4D@users.noreply.github.com>
2020-12-16 14:55:57 +0000

    Adding comments to OilTanckerFactory

--- src/main/java/es/ull/esit/factories/OilTankerFactory.java
+++ src/main/java/es/ull/esit/factories/OilTankerFactory.java
@@ -5,7 +5,17 @@ import es.ull.esit.transports.Transport;
 
 import java.awt.geom.Point2D;
 
+/**
+ *  @class OilTanckerFactory
+ *  @brief A concrete factory, which function is generate an OilTanker
+ *
+ */
+
 public class OilTankerFactory implements TransportFactory {
+    /**
+     * @brief Overrided method capable to create a new OilTanker
+     * @return new OilTanker
+     */
     @Override
     public Transport createTransport() {
         return new OilTanker();
