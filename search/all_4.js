64f29a5feedb35fbb7110a617b467ae162935208
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-16 14:16:34 +0000

    Deploy alu0101109251/radarLDH to github.com/alu0101109251/radarLDH.git:gh-pages

--- search/all_4.js
+++ search/all_4.js
@@ -6,7 +6,7 @@ var searchData=
   ['geomap_2ejava',['GeoMap.java',['../_geo_map_8java.html',1,'']]],
   ['getlatitude',['getLatitude',['../classes_1_1ull_1_1esit_1_1transports_1_1_transport.html#a7f3a983e9ee0fb372db68f391a572b73',1,'es::ull::esit::transports::Transport']]],
   ['getlongitude',['getLongitude',['../classes_1_1ull_1_1esit_1_1transports_1_1_transport.html#adb3953330ac35aad21195c92ef40641e',1,'es::ull::esit::transports::Transport']]],
-  ['getoceancoordinates',['getOceanCoordinates',['../classes_1_1ull_1_1esit_1_1utilities_1_1_water.html#aa41af38a32e5956694720f971cefc633',1,'es::ull::esit::utilities::Water']]],
+  ['getoceancoordinates',['getOceanCoordinates',['../classes_1_1ull_1_1esit_1_1utilities_1_1_water.html#a568ec46e14c240d888f4adbfffe9ac8b',1,'es::ull::esit::utilities::Water']]],
   ['gettransports',['getTransports',['../classes_1_1ull_1_1esit_1_1_console_main.html#a56852fb6b2c25671bd8c7ef3a19796e7',1,'es::ull::esit::ConsoleMain']]],
   ['gettype',['getType',['../classes_1_1ull_1_1esit_1_1transports_1_1_transport.html#af4ac2bd38c9c1fffdbc14abfdf3797be',1,'es::ull::esit::transports::Transport']]]
 ];
